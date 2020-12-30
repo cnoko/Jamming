8bc3b9b81ea1f3557c6f62dac2e7a65961842771
cnokoo@gmail.com <cnokoo@gmail.com>
2020-12-30 04:14:39 +0000

    ready to deploy

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,5 +1,5 @@
-const clientId = REACT_APP_CLIENT_ID;
-const redirectUri = 'http://localhost:3000/';
+const clientId = process.env.REACT_APP_CLIENT_ID;
+const redirectUri = process.env.REACT_APP_REDIRECT_URI;
 const _scopes = ['playlist-modify-public'];
 let _accessToken;
 const Spotify = {
