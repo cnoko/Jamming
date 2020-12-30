225d60f94555bf913818bf35542af5e40ff9119c
cnokoo@gmail.com <cnokoo@gmail.com>
2020-12-30 10:06:36 +0000

    build version

---
+++ build/precache-manifest.1deb5507d73d69754cacfb5a21ca11ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e421108f4988cfcb219ec07ea254960",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0000c22e988b5025b1a8",
+    "url": "/static/css/main.c454b34a.chunk.css"
+  },
+  {
+    "revision": "b0f597be5bf5280f10d7",
+    "url": "/static/js/2.87f56ebd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.87f56ebd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0000c22e988b5025b1a8",
+    "url": "/static/js/main.70177dcb.chunk.js"
+  },
+  {
+    "revision": "505e4dc665c09938bf7e",
+    "url": "/static/js/runtime-main.9075e973.js"
+  },
+  {
+    "revision": "37be7357a5f8583073df7e0aec787eaa",
+    "url": "/static/media/background_photo.37be7357.jpg"
+  }
+]);
